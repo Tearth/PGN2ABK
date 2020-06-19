fdc18039a265c8359247a68d36216d96f99e0772
Tearth <tearthdev@gmail.com>
2020-06-19 23:03:11 +0200

    Add initial project

---
+++ PGN2ABK/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PGN2ABK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
